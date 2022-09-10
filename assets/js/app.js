94ac0d7511d231bce5f32966655903458c98af26
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-09-10 10:13:36 +0300

    refactor div elements created earlier

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,54 @@
 // select sections with a class of section(all)
-const sections = document.querySelectorAll('.section');
+const sections = document.querySelectorAll(".section");
 //controls(parent section holding the icons)
-const sectBtns = document.querySelectorAll('.controls');
+const sectBtns = document.querySelectorAll(".controls");
 // buttons with a class of control for  individual buttons in controls
-const sectBtn = document.querySelectorAll('.control');
+const sectBtn = document.querySelectorAll(".control");
 // select body/main content
-const allSections = document.querySelector('.main-content');
+const allSections = document.querySelector(".main-content");
 
+function pageTransitions() {
+  // button click active class: get rid of class that is not active and
+  // display which is active
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
+  }
 
-function pageTransitions(){
-    // button click active class: get rid of class that is not active and 
-    // display which is active
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        })
+  // sections active class
+  allSections.addEventListener("click", (e) => {
+    // console.log (e.target); // confirm it's targeting
+    const id = e.target.dataset.id; // take the data-ids and store in a variable id
+    if (id) {
+      // remove selected from the other button
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      // hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const elements = document.getElementById(id);
+      elements.classList.add("active");
     }
-
-    // sections active class
-    allSections.addEventListener('click', (e) =>{
-        // console.log (e.target); // confirm it's targeting
-        const id = e.target.dataset.id; // take the data-ids and store in a variable id
-        if(id){
-            // remove selected from the other button
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-
-
-            // hide other sections
-            sections.forEach((section) =>{
-                section.classList.remove('active')
-            })
-
-            const elements = document.getElementById(id);
-            elements.classList.add('active');
-        }
-
-    })
+  });
 }
 
 pageTransitions();
 
 function renderHood(hood) {
-  // build animal
-  let card = document.createElement("li");
+  let card = document.createElement("div");
   card.className = "card";
   card.innerHTML = `
-   
-    <div class = "content">
     <img src = "${hood.image}">
         <h4>${hood.name}</h4>
         <p>
@@ -64,27 +61,22 @@ function renderHood(hood) {
           <a href="" class="main-btn">
           <span class="btn-text">Sell</span></a>
         </div>
-    </div>
-    
     `;
   // Add hood card to DOM
   document.querySelector("#display-hoodies").appendChild(card);
 }
 
-function getHoodies(){
-  fetch('http://localhost:3000/hoodies')
-  .then(response => response.json())
-  .then(hoodies => hoodies.forEach(hood => renderHood(hood)))
+function getHoodies() {
+  fetch("http://localhost:3000/hoodies")
+    .then((response) => response.json())
+    .then((hoodies) => hoodies.forEach((hood) => renderHood(hood)));
 }
 
 // sneakers
 function renderSneaker(sneaker) {
-  // build animal
-  let card = document.createElement("li");
+  let card = document.createElement("div");
   card.className = "card";
   card.innerHTML = `
-    
-    <div class = "content">
     <img src = "${sneaker.image}">
         <h4>${sneaker.name}</h4>
         <p>
@@ -97,27 +89,23 @@ function renderSneaker(sneaker) {
           <a href="" class="main-btn">
           <span class="btn-text">Sell</span></a>
         </div>
-    </div>
     `;
   // Add sneaker card to DOM
   document.querySelector("#display-sneakers").appendChild(card);
 }
 
-function getSneakers(){
-  fetch('http://localhost:3000/sneakers')
-  .then(response => response.json())
-  .then(sneakers => sneakers.forEach(sneaker => renderSneaker(sneaker)))
+function getSneakers() {
+  fetch("http://localhost:3000/sneakers")
+    .then((response) => response.json())
+    .then((sneakers) => sneakers.forEach((sneaker) => renderSneaker(sneaker)));
 }
 
 // shirts
 
 function renderShirt(shirt) {
-  // build animal
-  let card = document.createElement("li");
+  let card = document.createElement("div");
   card.className = "card";
   card.innerHTML = `
-    
-    <div class = "content">
     <img src = "${shirt.image}">
         <h4>${shirt.name}</h4>
         <p>
@@ -130,20 +118,19 @@ function renderShirt(shirt) {
           <a href="" class="main-btn">
           <span class="btn-text">Sell</span></a>
         </div>
-    </div>
     `;
   // Add shirt card to DOM
   document.querySelector("#display-shirts").appendChild(card);
 }
 
-function getShirts(){
-  fetch('http://localhost:3000/shirts')
-  .then(response => response.json())
-  .then(shirts => shirts.forEach(shirt => renderShirt(shirt)))
+function getShirts() {
+  fetch("http://localhost:3000/shirts")
+    .then((response) => response.json())
+    .then((shirts) => shirts.forEach((shirt) => renderShirt(shirt)));
 }
-function initialize(){
+function initialize() {
   getHoodies();
   getSneakers();
   getShirts();
 }
-initialize()
+initialize();
